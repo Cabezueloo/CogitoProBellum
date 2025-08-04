bbadd2a059edbc0eed88a8426b6ba39147d81834
Angel <angel@gmail.com>
2025-08-04 12:46:05 +0200

    Colors, pages, TODO information and contact, horarios, nosotros

---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+// tailwind.config.mjs
+export default {
+  darkMode: 'class', // ¡Importante!
+  content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
